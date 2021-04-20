cf3fa02e025cbf526c87b78640c993508d911085
Erick Velez <erickvelez7@gmail.com>
2021-04-19 23:43:03 -0700

    Add routes, endpoints, and set template engine in app.js

--- application/app.js
+++ application/app.js
@@ -2,19 +2,37 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const handlebars = require("express-handlebars");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const port = 3000;
+
+const index_router = require('./routes/index'),
+  post_image_router = require('./routes/post_image'),
+  registration_router = require('./routes/registration'),
+  image_post_router = require('./routes/image_post'),
+  login_router = require('./routes/login');
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+app.use("/public", express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index_router);
+app.use('/post_image', post_image_router);
+app.use('/registration', registration_router);
+app.use('/image_post', image_post_router);
+app.use('/login', login_router);
 
+app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars ({
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + '/views/partials'
+}));
+
+app.listen(port, function () {
+  console.log("Listening on port 3000");
+});
 module.exports = app;
