1a33a7bef4a5b4cdce02d3abfef1a48d0f27f9e8
Erick Velez <erickvelez7@gmail.com>
2021-04-19 23:39:49 -0700

    Add and update routs for hbs

---
+++ application/routes/image_post.js
@@ -0,0 +1,18 @@
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // placeholder for actual post data
+  res.render('main', {
+    layout: 'image_post',
+    post: {
+      title: "Example title",
+      description: "Example description",
+      image: "url"
+    }
+  });
+});
+
+module.exports = router;
--- application/routes/registration.js
+++ application/routes/registration.js
@@ -1,9 +1,11 @@
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('main', {layout: 'home'});
+  // res.sendFile('registration.html');
+  res.render('main', {layout: 'registration'});
 });
 
 module.exports = router;
